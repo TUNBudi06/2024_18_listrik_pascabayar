9e312f11062096fa07933eafd84e5a7e927ebfdb
TUNBudi06 <anjaytun2006@gmail.com>
2025-01-16 14:40:51 +0700

    add initial sweetalert,add livewire for toast and sweetalert

---
+++ app/Livewire/Notify/Alert.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Notify;
+
+use Livewire\Component;
+
+class Alert extends Component
+{
+    public function render()
+    {
+        return view('livewire.notify.alert');
+    }
+}
